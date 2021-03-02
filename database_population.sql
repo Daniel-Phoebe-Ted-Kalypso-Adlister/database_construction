894b7ece293fcfa6992cbd77d925fa5568238b0c
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-03-02 13:49:20 -0600

    feat: Added categories insert

--- database_population.sql
+++ database_population.sql
@@ -8,4 +8,12 @@ INSERT INTO ads(title, description, user_id) VALUES
 ('Chess Club', 'Come play chess with us! It takes two to tango.', 3),
 ('Study hall', 'Let''s ace this test togther', 2),
 ('Roommate wanted', 'Make sure you have manners', 3),
-('Kite flying', 'Wheather is nice this weekend', 1);
+('Kite flying', 'Weather is nice this weekend', 1);
+
+INSERT INTO categories(name) VALUES
+('For Sale'),
+('Club'),
+('Academic'),
+('Personal'),
+('Free'),
+('Recreational');
